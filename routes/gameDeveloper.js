fa68cdaef81c4342ccf9d430f3c67c99f0e91fed
pornpipatpt <pornpipat.pt@mail.kmutt.ac.th>
2021-09-12 16:11:38 +0700

    create gamedeveloper.js&&create get API

---
+++ routes/gameDeveloper.js
@@ -0,0 +1,18 @@
+const router = require("express").Router()
+const { PrismaClient } = require("@prisma/client")
+const { gamedeveloper } = new PrismaClient()
+
+
+
+router.get("/", async (req, res) => {
+    let allGameDeveloper = await gamedeveloper.findMany({
+        include: {
+            keygames:true
+        }
+    })
+
+     return res.send({ data: allGameDeveloper })
+
+})
+
+module.exports = router
